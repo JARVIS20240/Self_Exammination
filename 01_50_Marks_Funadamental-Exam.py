2aab6e32b1a5b1b488f74a1e4d639b6a02a86c55
JARVIS20240 <mistrykaran2911@gmail.com>
2026-01-24 20:29:25 +0530

    solve q1

--- 01_50_Marks_Funadamental-Exam.py
+++ 01_50_Marks_Funadamental-Exam.py
@@ -1,1 +1,31 @@
-print("Hello World")
+# 1st Paper:
+"""
+🔹 Section A — Python Fundamentals (20 Marks)
+"""
+# Q1: 
+"""Q1. Input, Conditions, Logic (5 Marks)
+Write a program that:
+Takes an integer input n
+
+Prints:
+"Positive Even" if n is positive and even
+"Positive Odd" if n is positive and odd
+"Negative" if n is negative
+"Zero" if n == 0
+📌 Marks Deduction if logic is repetitive or poorly structured."""
+
+def check_num():
+    try:
+        number = int(input("Enter Number: "))
+        if (number > 0 ) and (number%2==0):
+            print(f"{number} is Positive and Even Number.")
+        elif (number > 0 ) and (number%2!=0):
+            print(f"{number} is Positive and Odd Number.")
+        elif (number < 0):
+            print(f"{number} is Negative.")
+        else:
+            print(f"Zero")
+
+    except ValueError:
+        print("Enter Valid Input.")
+check_num()
